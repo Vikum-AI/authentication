14c42e7387df664ac279c9e589ec957a3c8a9c1c
Vikum-AI <63595074+Vikum-AI@users.noreply.github.com>
2023-03-12 01:38:17 +0530

    Simple jwt package

--- physicsstudios/physicsstudios/settings.py
+++ physicsstudios/physicsstudios/settings.py
@@ -69,9 +69,8 @@ REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',
-    ),
-    # 'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser', ),
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
 }
 
 TEMPLATES = [
