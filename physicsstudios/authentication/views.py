53d7bbe92dcf240aef2f562a886e8ac408b2ef04
Vikum-AI <63595074+Vikum-AI@users.noreply.github.com>
2023-03-12 01:24:23 +0530

    Remove unused imports

--- physicsstudios/authentication/views.py
+++ physicsstudios/authentication/views.py
@@ -1,10 +1,6 @@
-from django.shortcuts import render
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework.generics import CreateAPIView, GenericAPIView
+from rest_framework.generics import CreateAPIView
 from rest_framework import permissions
 from django.contrib.auth import get_user_model
-from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
 
 
 from .serializers import RegisterSerializer
